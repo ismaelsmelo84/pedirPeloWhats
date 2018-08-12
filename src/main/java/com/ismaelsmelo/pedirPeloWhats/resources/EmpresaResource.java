8dafb2699165b252cb422b81537bb7f344b17aa8
Ismael Melo <ismaelsmelo84@gmail.com>
2018-08-12 12:47:11 -0300

    Testando a primeira classe de domínio - Empresa

--- src/main/java/com/ismaelsmelo/pedirPeloWhats/resources/EmpresaResource.java
+++ src/main/java/com/ismaelsmelo/pedirPeloWhats/resources/EmpresaResource.java
@@ -1,15 +1,28 @@
 package com.ismaelsmelo.pedirPeloWhats.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ismaelsmelo.pedirPeloWhats.domain.Empresa;
 
 @RestController
 @RequestMapping(value="/empresas")
 public class EmpresaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando!!!";
+	public List<Empresa> listar() {
+		
+		Empresa emp1 = new Empresa(1, "Pizzaria Red Fuego");
+		Empresa emp2 = new Empresa(2, "Pastel da Ilha");
+		
+		List<Empresa> lista = new ArrayList<>();
+		lista.add(emp1);
+		lista.add(emp2);
+		
+		return lista;
 	}
 }
