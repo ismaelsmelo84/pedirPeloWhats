b2be5ef4fde73ad7c42c355ca03926d7a684d6b1
Ismael Melo <ismaelsmelo84@gmail.com>
2018-08-12 11:37:38 -0300

    Projeto criado

---
+++ src/main/java/com/ismaelsmelo/pedirPeloWhats/PedirPeloWhatsApplication.java
@@ -0,0 +1,12 @@
+package com.ismaelsmelo.pedirPeloWhats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedirPeloWhatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedirPeloWhatsApplication.class, args);
+	}
+}
