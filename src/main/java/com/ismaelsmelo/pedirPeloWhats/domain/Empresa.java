03135a2820d455ba0599b2f58e3dda6b6a9eff0d
Ismael Melo <ismaelsmelo84@gmail.com>
2018-08-12 14:51:25 -0300

    Banco de dados H2 e criação automática da base de dados

--- src/main/java/com/ismaelsmelo/pedirPeloWhats/domain/Empresa.java
+++ src/main/java/com/ismaelsmelo/pedirPeloWhats/domain/Empresa.java
@@ -2,9 +2,14 @@ package com.ismaelsmelo.pedirPeloWhats.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Empresa implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	private Integer codigo;
 	private String nome;
 	
